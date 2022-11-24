7c873a872ef5d5e194dc9dcce73cbad78618ea78
Kckamalesh9 <kckamalesh95@gmail.com>
2022-11-24 18:53:32 +0530

    C Added

--- src/org/sam/Sample.java
+++ src/org/sam/Sample.java
@@ -22,11 +22,16 @@ public class Sample {
 
 	public static void main(String[] args) {
 
-		int a=10,b=15;
-		System.out.println(a+b);
-		System.out.println(b-a);
-		System.out.println(a*b);
-		System.out.println(b/a);
+		Sample sa = new Sample();
+		int a = 10, b = 15;
+		sa.add();
+		System.out.println(a + b);
+		sa.sub();
+		System.out.println(b - a);
+		sa.mul();
+		System.out.println(a * b);
+		sa.div();
+		System.out.println(b / a);
 
 	}
 }
